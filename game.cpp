2cdb9d66625288dee6f1fba4d4679dabf53482bf
BoomyF9876 <710767850@qq.com>
2025-03-23 21:52:56 -0400

    Change sth

--- game.cpp
+++ game.cpp
@@ -1,17 +1,17 @@
 #include "game.h"
 #include <iostream>
 #include <cstdlib> // for the rand() function
-
+using namespace std;
 
 // Global variables to keep track of the game statistics 
 int playerWins = 0;
 int dealerWins = 0;
 int ties = 0;
 
-void shuffleDeck(std::vector<int>& deck) {
+void shuffleDeck(vector<int>& deck) {
     for (size_t i = 0; i < deck.size(); ++i) {
         size_t randomIndex = rand() % deck.size();
-        std::swap(deck[i], deck[randomIndex]);
+        swap(deck[i], deck[randomIndex]);
     }
 
     // A bit of info about the "rand()" function.
@@ -39,35 +39,33 @@ void shuffleDeck(std::vector<int>& deck) {
 
 // TODO (1): Complete the following function 
 // (the description is in the header file)
-int dealCard(std::vector<int>& deck) {
-    // HINT: look up std::vector methods
+int dealCard(vector<int>& deck) {
+    // HINT: look up vector methods
     int next_card = deck[deck.size() - 1];
     deck.pop_back();
 
-    return next_card; // You may want to change this return value 
+    return next_card;
 }
 
 
-int playerTurn(std::vector<int>& deck) {
+int playerTurn(vector<int>& deck) {
     int playerTotal = 0;
     char choice = ' ';
 
-    std::cout << "Your turn!" << std::endl;
+    cout << "Your turn!" << endl;
 
     // Deal two cards initially
     playerTotal += dealCard(deck);
-    std::cout << "You drew a " << playerTotal << ". Your total: " << playerTotal << std::endl;
+    cout << "You drew a " << playerTotal << ". Your total: " << playerTotal << endl;
     
     int newCard = dealCard(deck);
     playerTotal += newCard;
-    std::cout << "You drew a " << newCard << ". Your total: " << playerTotal << std::endl;
+    cout << "You drew a " << newCard << ". Your total: " << playerTotal << endl;
    
     // In the very rare case of drawing two aces (11) 
     if (playerTotal > 21) {
         return playerTotal;
     }
-
-    // TODO (2): Complete the following loop and return the player's total
 
     // Inside the loop, the player should be asked to 
     // either "hit" (draw a card) or "stay" (keep their current hand).
@@ -77,13 +75,13 @@ int playerTurn(std::vector<int>& deck) {
     // or until the player's total exceeds 21 (bust). 
 
     while (choice != 's') {
-        std::cout<<"Should you choose to draw a card (type h) or keep your current hand (type s)?\n";
-        std::cin>>choice;
+        cout<<"Should you choose to draw a card (type h) or keep your current hand (type s)?\n";
+        cin>>choice;
 
         if (choice == 'h') {
             newCard = dealCard(deck);
             playerTotal += newCard;
-            std::cout << "You drew a " << newCard << ". Your total: " << playerTotal << std::endl;
+            cout << "You drew a " << newCard << ". Your total: " << playerTotal << endl;
         }
 
         if (playerTotal > 21) {
@@ -91,27 +89,27 @@ int playerTurn(std::vector<int>& deck) {
         }
     }
 
-    return playerTotal; // You may want to change this return value 
+    return playerTotal;
 }
 
 
 // TODO (3) : Complete the following function
 // (the description is in the header file)
-int dealerTurn(std::vector<int>& deck) {
+int dealerTurn(vector<int>& deck) {
    
     // HINT: Complete the previous function first. 
     // The content of this function is very similar to the previous one.
 
     int dealerTotal = 0;
-    std::cout << "Dealer's turn!" << std::endl;
+    cout << "Dealer's turn!" << endl;
 
     // Deal two cards initially
     dealerTotal += dealCard(deck);
-    std::cout << "Dealer drew a " << dealerTotal << ". Dealer's total: " << dealerTotal << std::endl;
+    cout << "Dealer drew a " << dealerTotal << ". Dealer's total: " << dealerTotal << endl;
     
     int newCard = dealCard(deck);
     dealerTotal += newCard;
-    std::cout << "Dealer drew a " << newCard << ". Dealer's total: " << dealerTotal << std::endl;
+    cout << "Dealer drew a " << newCard << ". Dealer's total: " << dealerTotal << endl;
    
     // In the very rare case of drawing two aces (11) 
     if (dealerTotal > 21) {
@@ -119,9 +117,9 @@ int dealerTurn(std::vector<int>& deck) {
     }
 
     while (dealerTotal < 17) {
+        cout << "Dealer drew a " << newCard << ". Dealer's total: " << dealerTotal << endl;
         newCard = dealCard(deck);
         dealerTotal += newCard;
-        std::cout << "Dealer drew a " << newCard << ". Dealer's total: " << dealerTotal << std::endl;
     }
 
     return dealerTotal; // You may want to change this return value 
@@ -132,23 +130,32 @@ void determineWinner(int playerTotal, int dealerTotal) {
 
     // - If the player has busted (gone over 21), they lose automatically, 
     //   regardless of the dealer's score.
+    if (playerTotal > 21) {
+        dealerWins++;
+    }
     
     // - If the dealer has busted, the player wins, unless the player also busted.
+    else if (dealerTotal > 21) {
+        playerWins++;
+    } 
     
     // - If neither player nor dealer busts, the total closest to 21 wins.
-
+    else if (playerTotal > dealerTotal) {
+        playerWins++;
+    }
+    else if (playerTotal < dealerTotal) {
+        dealerWins++;
+    }
     // - If both have the same total, it is a tie.
     // Update the global variables at the top accordingly 
-    if (playerTotal > 21) dealerWins++;
-    else if (dealerTotal > 21) playerWins++;
-    else if (playerTotal > dealerTotal) playerWins++;
-    else if (playerTotal < dealerTotal) dealerWins++;
-    else ties++;
+    else {
+        ties++;
+    }
 }
 
 void playGame() {
     // Vector representing the deck with numbers
-    std::vector<int> deck = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,  // First set
+    vector<int> deck = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,  // First set
                               2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 }; // Second set (simulating two decks)
 
 
@@ -156,14 +163,14 @@ void playGame() {
     // 5.1 Shuffle the deck
     shuffleDeck(deck);
     // 5.2 Make the player play, followed by the dealer
-    int playerPoint = playerTurn(deck);
-    int dealerPoint = dealerTurn(deck);
+    int player = playerTurn(deck);
+    int dealer = dealerTurn(deck);
 
     // 5.3 Determine the winner
-    determineWinner(playerPoint, dealerPoint);
+    determineWinner(player, dealer);
 
 
     // Prints the game stats
-    std::cout << "Stats: Player Wins: " << playerWins << ". Dealer Wins: " << dealerWins << ". Ties: " << ties << std::endl;
+    cout << "Stats: Player Wins: " << playerWins << ". Dealer Wins: " << dealerWins << ". Ties: " << ties << endl;
     
 }
